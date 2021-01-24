807e76bb32e50bad2ea072fd4d763838d886f6e4
jrinabnit <jrinabnit@gmail.com>
2021-01-24 11:54:39 +0800

    adding google auth & logout

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,12 +1,19 @@
 import React, { useState} from 'react';
 import { Avatar, Button, Paper, Grid, Typography, Container } from '@material-ui/core'
-import LockOutlinedIcon from '@material-ui/icons/LockOutlined'
-import Input from './Input'
+import { GoogleLogin } from 'react-google-login';
+import { useDispatch } from 'react-redux'
+import { useHistory } from 'react-router-dom'
+
+import Icon from './icon';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Input from './Input';
 
 import useStyles from './styles'
 
 const Auth = () => {
     const classes = useStyles();
+    const dispatch = useDispatch();
+    const history = useHistory();
     const [ showPassword, setShowPassword ] = useState(false);
     const [isSignup, setIsSignup] = useState(false)
     const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword)
@@ -22,6 +29,22 @@ const Auth = () => {
     const switchMode = () => {
         setIsSignup(prevIsSignup => !prevIsSignup);
         handleShowPassword(false)
+    }
+
+    const googleSuccess = async (res) => {
+        const result = res?.profileObj;
+        const token = res?.tokenId;
+
+        try {
+            dispatch({ type: 'AUTH', data: { result, token }})
+            history.push('/')
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const googleFailure = (error) => {
+        console.log('Google Sign In was unsuccessful. Try again later')
     }
 
 	return (
@@ -44,15 +67,34 @@ const Auth = () => {
                             <Input name='password' label='Password' handleChange={handleChange} type={showPassword ? 'text' : 'password'} handleShowPassword={handleShowPassword} /> 
                             { isSignup && <Input name="confirmPassword" label="Confirm Password" handleChange={handleChange} type="password" /> }
                     </Grid>
-                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit}>
+                    <Button type='submit' fullWidth variant='contained' color='primary' className={classes.submit} >
                         {isSignup ? 'Sign In' : 'Sign Up'}
                     </Button>
+                    <GoogleLogin 
+                        clientId='278767358758-ikdimmnbu1iuaeqtnr0vnqpqfof2k2bj.apps.googleusercontent.com'
+                        render={(renderProps) => (
+                            <Button className={classes.googleButton} 
+                            color='primary' 
+                            fullWidth 
+                            onClick={renderProps.onClick} 
+                            disabled={renderProps.disabled} 
+                            startIcon={<Icon />} 
+                            variant='contained'>
+                            Google Sign In 
+                            </Button>
+                        )}
+                        onSuccess={googleSuccess}
+                        onFailure={googleFailure}
+                        cookiePolicy='single_host_origin'
+                        />
+                  
                     <Grid container justify='flex-end'>
                                 <Grid item>
                                     <Button onClick={switchMode}>
                                         { isSignup ? "Already have an account? Sign In" : "Don't have an account? Sign Up" }
                                     </Button>
                                 </Grid>
+                                
                     </Grid>
                 </form>
             </Paper>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { AppBar, Typography, Toolbar, Button, Avatar } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+import { Link, useHistory, useLocation } from 'react-router-dom';
 
 import useStyles from './styles';
 import memories from '../../images/memories.png';
 
 const Navbar = () => {
 	const classes = useStyles();
-	const user = null;
+	const dispatch = useDispatch();
+	const history = useHistory();
+	const location = useLocation();
+	const [ user, setUser ] = useState(JSON.parse(localStorage.getItem('profile')));
+
+	const logout = () => {
+		dispatch({ type: 'LOGOUT' });
+		history.push('/');
+		setUser(null);
+	};
+
+	useEffect(() => {
+		const token = user?.token;
+
+		// if (token) {
+		// 	const decodedToken = decode(token);
+	  
+		// 	if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+		//   }
+
+		setUser(JSON.parse(localStorage.getItem('profile')));
+
+	}, [location])
 
 	return (
 		<AppBar className={classes.appBar} position='static' color='inherit'>
@@ -26,12 +49,12 @@ const Navbar = () => {
 						<Typography className={classes.userName} variant='h6'>
 							{user.result.name}
 						</Typography>
-						<Button variant='contained' className={classes.logout} color='secondary'>
+						<Button variant='contained' className={classes.logout} color='secondary' onClick={logout}>
 							Logout
 						</Button>
 					</div>
 				) : (
-					<Button component={Link} to='/auth' variant='contained' color='primary' />
+					<Button component={Link} to='/auth' variant='contained' color='primary'>Sign In</Button>
 				)}
 			</Toolbar>
 		</AppBar>
